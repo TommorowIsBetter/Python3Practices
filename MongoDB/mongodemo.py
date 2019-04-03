7c79177587a8ac612386eb29944900f7bf9d28a8
TommorowIsBetter <841331749@qq.com>
2019-04-03 15:29:00 +0800

    Add the contents of collections of the MongoDB and then save them to the collection that is required.

--- MongoDB/mongodemo.py
+++ MongoDB/mongodemo.py
@@ -5,3 +5,26 @@
 @ide:PyCharm
 @time:2019/4/3 13:05
 """
+
+# 用于读取MongoDB数据库里面的指定集合内容，然后加入到新的集合中去。
+from pymongo import MongoClient
+# 连接MongoDB的目标Ip地址，端口号为27017
+client = MongoClient('192.168.1.103', 27017)
+# 这里选择MongoDB的test的数据库
+db = client.test
+# 定义一个列表
+dataset = []
+# 查询col的数据并获得游标
+cursor = db.col2.find({})
+for doc in cursor:
+    # 把_id属性去除，后面插入到新集合的时候会自动生成新的_id
+    doc.pop('_id')
+    # 获取到的数据加入到列表中
+    dataset.append(doc)
+print("append over")
+print(dataset)
+# 对刚获得的列表进行遍历，然后把每个元素插入的新的集合col中
+for doc in dataset:
+    db.col.insert_one(doc)
+
+
