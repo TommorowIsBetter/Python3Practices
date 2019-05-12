3cb350277a280a6f51c9d705177a2d5fd87aff89
TommorowIsBetter <841331749@qq.com>
2019-05-12 14:53:47 +0800

    Add three methods to read csv files in csv_util.py.

--- CSV/csv_util.py
+++ CSV/csv_util.py
@@ -6,11 +6,43 @@
 @time:2019/5/9 16:41
 """
 import csv
-data = [['name', 'age'],
-        ['Bob', 14],
-        ['Tom', 23],
-        ['Jerry', '18']]
-with open('data.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    for row in data:
-        writer.writerow(row)
+
+
+# 写入数据到csv文件中
+def save_csv():
+    data = [['name', 'age'],
+            ['Bob', 14],
+            ['Tom', 23],
+            ['Jerry', '18']]
+    with open('data.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in data:
+            writer.writerow(row)
+
+
+# 一次性读取整个csv文件
+def read_csv_all():
+    with open('data.csv', 'r') as file_object:
+        for line in file_object:
+            # 调用rstrip()消除空白行
+            print(line.rstrip())
+
+
+# 逐行读取csv文件
+def read_csv_line():
+    with open('data.csv', 'r') as file_object:
+        for line in file_object:
+            # 调用rstrip()消除空白行
+            print(line.rstrip())
+
+
+# 创建一个包含各行内容的列表
+def read_csv_line_list():
+    with open('data.csv', 'r') as file_object:
+        lines = file_object.readlines()
+    for line in lines:
+        print(line.rstrip())
+
+
+read_csv_all()
+
