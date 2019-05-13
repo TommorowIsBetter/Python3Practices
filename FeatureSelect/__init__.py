f3ebb32faf0e53a1f516b1bdfd5209dc0a6d5dd4
TommorowIsBetter <841331749@qq.com>
2019-05-13 11:07:23 +0800

    Add FeatureSelect directory and add mean_decrease_accuracy.py and mean_decrease_impurity.py in it.

---
+++ FeatureSelect/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author:Wang Yan
+@ide:PyCharm
+@time:2019/5/10 21:32
+"""
---
+++ FeatureSelect/mean_decrease_impurity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author:Wang Yan
+@ide:PyCharm
+@time:2019/5/10 21:32
+"""
+from sklearn.datasets import load_boston
+from sklearn.ensemble import RandomForestRegressor
+# Load boston housing dataset as an example
+boston = load_boston()
+X = boston["data"]
+Y = boston["target"]
+names = boston["feature_names"]
+rf = RandomForestRegressor()
+rf.fit(X, Y)
+print("Features sorted by their score:")
+print(sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names), reverse=True))
