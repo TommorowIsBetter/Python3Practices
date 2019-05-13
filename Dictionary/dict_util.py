d37ef659b4a7aeb191c6259787cd5a3a967217cb
TommorowIsBetter <841331749@qq.com>
2019-05-13 11:19:47 +0800

    Update dict_util.py in Dictionary directory.

--- Dictionary/dict_util.py
+++ Dictionary/dict_util.py
@@ -7,4 +7,15 @@
 """
 
 dictionary = {'color': 'green', 'points': 5}
+# 查询一个键值对
+print(dictionary['color'])
+print(dictionary)
+# 添加一个键值对
+dictionary['name'] = "name_test"
+print(dictionary)
+# 更新一个键值对
+dictionary['color'] = 'red'
+print(dictionary)
+# 删除一个键值对
+del dictionary['points']
 print(dictionary)
