ea65dffc48d4a9932e7d91ab1447f45088a734f0
TommorowIsBetter <841331749@qq.com>
2019-06-29 16:04:57 +0800

    Update List_Demo.py in List directory.

--- List/List_Demo.py
+++ List/List_Demo.py
@@ -10,5 +10,12 @@ list1 = [1, 2, 3]
 # 列表里面的元素组合
 combination = itertools.combinations(list1, 2)
 print(list(combination))
+# 列表里面的元素排列
 permutation = itertools.permutations(list1, 2)
 print(list(permutation))
+
+list2 = [2, 19, 3, 26]
+# 打印最大值的位置
+print(list2.index(max(list2)))
+# 打印最小值的位置
+print(list2.index(min(list2)))
