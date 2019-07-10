ec26ddd7076ff84a7209ccf8f44094225f18d43b
TommorowIsBetter <841331749@qq.com>
2019-07-10 16:35:19 +0800

    Update dict_util.py in Dictionary directory.

--- Dictionary/dict_util.py
+++ Dictionary/dict_util.py
@@ -5,7 +5,7 @@
 @ide:PyCharm
 @time:2019/5/12 15:23
 """
-
+import json
 dictionary = {'color': 'green', 'points': 5}
 # 查询一个键值对
 print(dictionary['color'])
@@ -19,3 +19,22 @@ print(dictionary)
 # 删除一个键值对
 del dictionary['points']
 print(dictionary)
+
+
+# 把键值对写入txt文件中
+str_name = 'WangYan'
+str_sex = 'man'
+file_name = 'WangYan' + '.txt'
+dic = {'name': str_name,
+       'sex': str_sex}
+js = json.dumps(dic)
+file = open(file_name, 'w')
+file.write(js)
+file.close()
+
+# 读取txt中的字典
+file = open(file_name, 'r')
+js = file.read()
+dic = json.loads(js)
+print(dic)
+file.close()
