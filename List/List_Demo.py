47418809017d785fb8413f83eecfdd5f0a52ce97
TommorowIsBetter <841331749@qq.com>
2019-07-12 15:09:11 +0800

    Update list_Demo.py in List directory.

--- List/List_Demo.py
+++ List/List_Demo.py
@@ -45,11 +45,8 @@ for list_A in list_permutation:
 
 
 # 这里主要是为了解决类别不平衡问题，删除部分类别较多的项目
-list_input = [[12, 11], [33, 22], [12, 45], [8, 9], [22, -1], [44, 88]]
-list_label = [2, 2, 2, 1, 2, 3]
-# 统计列表中每个元素出现的个数,并且以字典形式保存
-res = dict(Counter(list_label))
-print(res)
+list_input_ = [[12, 11], [10, 10], [33, 22], [12, 45], [8, 9], [22, -1], [44, 88], [50, 50]]
+list_label_ = [2, 2, 2, 2, 1, 2, 3, 3]
 
 
 # 求重复的某个元素的索引值
@@ -58,21 +55,37 @@ def unique_index(L, f):
     return [i for (i, v) in enumerate(L) if v == f]
 
 
-# 输入待测试重复数字
-list_index = unique_index(list_label, 2)
-print(list_index)
-# 输入待组合的元素数量
-combination = itertools.combinations(list_index, 3)
-list_combination = list(combination)
-print(list_combination)
-list_len = len(list_combination)
-print('list_len', list_len)
-random_number = random.randint(0, list_len - 1)
-print("随机数的值：", random_number)
-del_list = list_combination[random_number]
-print(del_list)
-for i in del_list:
-    del list_input[i]
-    del list_label[i]
-print(list_input)
-print(list_label)
+def delete_repeat(list_input, list_label):
+    # 统计列表中每个元素出现的个数,并且以字典形式保存
+    res = dict(Counter(list_label))
+    list_value = sorted(res.items(), key=lambda item: item[1])
+    # 输入待测试重复数字
+    list_index = unique_index(list_label, 2)
+    # 输入待组合的元素数量
+    combination = itertools.combinations(list_index, list_value[-1][1] - list_value[-2][1])
+    list_combination = list(combination)
+    list_len = len(list_combination)
+    random_number = random.randint(0, list_len - 1)
+    del_list = list_combination[random_number]
+    new_list_input = []
+    new_list_label = []
+    for i in range(len(list_input)):
+        if not(i in del_list):
+            new_list_input.append(list_input[i])
+            new_list_label.append(list_label[i])
+    list_input = new_list_input
+    list_label = new_list_label
+    return list_input, list_label
+
+
+print(delete_repeat(list_input_, list_label_))
+
+# 删除多个指定位置的列表
+list_test = [2, 1, 4, 5, 8, 12]
+remove_index = [1, 2, 5]
+new_list = []
+for i in range(len(list_test)):
+    if not(i in remove_index):
+        new_list.append(list_test[i])
+list_test = new_list
+print(list_test)
