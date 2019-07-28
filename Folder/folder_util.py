71eed429947a6072f80d275963b672a4bdb72b67
wangyan841331749 <841331749@qq.com>
2019-07-28 16:16:53 +0800

    Update the folder_util.py.

--- Folder/folder_util.py
+++ Folder/folder_util.py
@@ -6,5 +6,15 @@
 @time:2019/7/19 20:52
 """
 import os
-dir = "test_folder"
+dir = "./test_folder/"
 print(len(os.listdir(dir)))
+
+
+# 计算文件夹下及其子文件夹下的所有文件的个数
+def count_file_number(dir):
+    file_count = 0
+    for dir_path, dir_names, file_names in os.walk(dir):
+        for file in file_names:
+            file_count = file_count + 1
+    return file_count
+print(count_file_number(dir))
