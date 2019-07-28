74634753de366153ccb2c7e3a4f4952fe27c20a3
wangyan841331749 <841331749@qq.com>
2019-07-28 11:02:31 +0800

    Update the re_util.py and str_util.py.

--- Re/re_util.py
+++ Re/re_util.py
@@ -7,6 +7,7 @@
 """
 
 import re
+
 string = "2018年5月15日 8:29"
 # 目标是取出里面的具体时间
 result = re.findall(".*日 (.*)", string)
@@ -23,3 +24,25 @@ number1 = re.findall("\d*\d", last_line)
 number2 = re.findall("hserver(.*)", last_line)
 print(number1[0])
 print(number2[0])
+
+string_name = "20190727080550_6_19.928.png"
+# 目标是取出19.928这个具体的数字
+index = string_name.split("_")[1]
+index = int(index)
+if index < 10:
+    hover_time = re.findall("(?<=\d{14}_\d_).*?(?=\.png)", string_name)
+elif 10 <= index < 100:
+    hover_time = re.findall("(?<=\d{14}_\d{2}_).*?(?=\.png)", string_name)
+elif 100 <= index < 1000:
+    hover_time = re.findall("(?<=\d{14}_\d{3}_).*?(?=\.png)", string_name)
+elif 1000 <= index < 10000:
+    hover_time = re.findall("(?<=\d{14}_\d{4}_).*?(?=\.png)", string_name)
+elif 10000 <= index < 100000:
+    hover_time = re.findall("(?<=\d{14}_\d{5}_).*?(?=\.png)", string_name)
+print(hover_time)
+
+
+string_name_1 = "20190727080550_6_19.928.png"
+# 目标是获取20190727080550_6_19.928这个子字符串
+pre_name = re.findall(".*?(?=\.png)", string_name_1)[0]
+print(pre_name)
--- Str/str_util.py
+++ Str/str_util.py
@@ -8,3 +8,9 @@
 # 获取路径的文件名字
 path = "K:/Project/FilterDriver/DriverCodes/hello.txt"
 print(path.split("/")[-1].split(".")[0])
+
+
+# 第二个参数为 1，返回两个参数列表
+str_name = "Google#Runoob#Taobao#Facebook"
+name = str_name.split("#", 2)
+print(name)
