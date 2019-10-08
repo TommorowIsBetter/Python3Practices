a280c52fd04488ba393f0d53f185135333a6be6f
wangyan841331749 <841331749@qq.com>
2019-10-08 13:15:16 +0800

    Update the HoughLinePractice.py and fix some more lines problem.

--- OpenCV/LinePractice/HoughLinePractice.py
+++ OpenCV/LinePractice/HoughLinePractice.py
@@ -14,16 +14,38 @@ edges = cv2.Canny(gray, 50, 150)
 # 最低线段的长度，小于这个值的线段被抛弃
 minLineLength = 100
 # 线段中点与点之间连接起来的最大距离，在此范围内才被认为是单行
-maxLineGap = 3
+maxLineGap = 5
 # 100阈值，累加平面的阈值参数，即：识别某部分为图中的一条直线时它在累加平面必须达到的值，低于此值的直线将被忽略。
 lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+lines_list = []
 for i in range(len(lines)):
     for x1, y1, x2, y2 in lines[i]:
-        print(x1, y1, x2, y2)
+        lines_list.append([x1, y1, x2, y2])
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        # 裁剪坐标为[y0:y1, x0:x1]
-        cropped = img[y1 - 50:y1, x1:x2]
-        cv2.imwrite(str(i) + ".jpg", cropped)
+# 把所有获取到的线段的纵坐标获取到
+lines_y = []
+for i in range(len(lines_list)):
+    lines_y.append(lines_list[i][1])
+# 删除所有多余的线段，有的是因为错误认为两条线段
+del_lines_list = []
+for i in range(len(lines_y)):
+    for j in range(len(lines_y))[i + 1:]:
+        if abs(lines_y[i] - lines_y[j]) <= 3:
+            del_lines_list.append(j)
+del_lines_list = list(set(del_lines_list))
+new_list = []
+for i in range(len(lines_list)):
+    if i not in del_lines_list:
+        new_list.append(lines_list[i])
+lines_list = new_list
+# 对获取到的线段进行裁剪
+count= 0
+for i in lines_list:
+    # 裁剪坐标为[y0:y1, x0:x1]
+    cropped = img[i[1] - 50:i[1], i[0]:i[2]]
+    count += 1
+    cv2.imwrite(str(count) + ".jpg", cropped)
+# 显示图片
 cv2.imshow("edges", edges)
 cv2.imshow("lines", img)
 cv2.waitKey()
