82cfee6bb5f2c27aa239165b3cfc158752d23ce7
wangyan841331749 <841331749@qq.com>
2019-12-22 20:21:21 +0800

    添加判断一个控件区域是否是图片的功能

--- Image/Rectangle/rectangle.py
+++ Image/Rectangle/rectangle.py
@@ -57,6 +57,40 @@ def cal_contrast(img):
     return image_contrast
 
 
+def is_image_colorfulness(image_, threshold_):
+    # 判断控件区域是否是图片
+    # 将图片分为B,G,R三部分（注意，这里得到的R、G、B为向量而不是标量）
+    (B, G, R) = cv2.split(image_.astype("float"))
+    # rg = R - G
+    rg = np.absolute(R - G)
+    # yb = 0.5 * (R + G) - B
+    yb = np.absolute(0.5 * (R + G) - B)
+    # 计算rg和yb的平均值和标准差
+    (rbMean, rbStd) = (np.mean(rg), np.std(rg))
+    (ybMean, ybStd) = (np.mean(yb), np.std(yb))
+    # 计算rgyb的标准差和平均值
+    stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+    meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+    # 返回颜色丰富度C
+    if stdRoot + (0.3 * meanRoot) > threshold_:
+        return True
+    else:
+        return False
+
+
+def is_picture(region_,  node_):
+    # 判断控件是否是图片
+    # 通过类名判断是否是控件
+    if 'Image' in node_['className']:
+        return True, (0, 0, 255)
+    # 通过控件区域的颜色分布判断是否是控件
+    elif is_image_colorfulness(region_, 55):
+        return True, (0, 255, 0)
+    else:
+        # 不是图片控件则返回False
+        return False, (255, 0, 0)
+
+
 def rectangle_and_get_information(json_file_, picture_file_):
     # 获取局部图片的特征信息，json_file_为json的路径，picture_file_为picture的路径
     # 色调值初始值
@@ -77,40 +111,35 @@ def rectangle_and_get_information(json_file_, picture_file_):
         content = json.load(data_file)
         nodes = content['nodes']
         for node in nodes:
-            if 0 < node['inScreenBounds']['bottom'] < 1184 and 0 < node['inScreenBounds']['top'] < 1184 and \
-                    0 < node['inScreenBounds']['left'] < 720 and 0 < node['inScreenBounds']['right'] < 720 and \
-                    node['inScreenBounds']['top'] < node['inScreenBounds']['bottom'] and node['inScreenBounds']['left']\
-                    < node['inScreenBounds']['right']:
-                #  这个主要用于在原图中显示已经被选中的矩形框
-                #  (left, top),(right,bottom) 按照这个顺序，具体的数值通过在json文件里面进行获取
-                draw_0 = cv2.rectangle(image, (node['inScreenBounds']['left'], node['inScreenBounds']['top']),
-                                       (node['inScreenBounds']['right'], node['inScreenBounds']['bottom']),
-                                       (0, 0, 255), 3, 4, 0)
+            if 0 <= node['inScreenBounds']['bottom'] <= content['event']['screenHeight'] and 0 <= node['inScreenBounds']['top'] <= content['event'] ['screenHeight'] and \
+                    0 <= node['inScreenBounds']['left'] <= content['event']['screenWidth'] and 0 <= node['inScreenBounds']['right'] <= content['event']['screenWidth'] and \
+                    node['inScreenBounds']['top'] < node['inScreenBounds']['bottom'] and \
+                    node['inScreenBounds']['left'] < node['inScreenBounds']['right']:
                 # 对选中的局部图片进行分析 [top:bottom, left:right]按照这个顺序进行放置
                 region = image[node['inScreenBounds']['top']:node['inScreenBounds']['bottom'],
-                               node['inScreenBounds']['left']:node['inScreenBounds']['right']]
-                print(node['inScreenBounds']['top'])
-                print(node['inScreenBounds']['bottom'])
-                print(node['inScreenBounds']['left'])
-                print(node['inScreenBounds']['right'])
-                print('--------------')
-                cv2.imshow("a", region)
-                h_s_v = cv2.cvtColor(region, cv2.COLOR_BGR2HSV)
-                h, s, v = cv2.split(h_s_v)
-                # 1.获取色调值
-                h_value += round(np.sum(h), 2)
-                # 2.获取饱和度值
-                s_value += round(np.sum(s), 2)
-                # 3.获取明度值
-                v_value += round(np.sum(v), 2)
-                # 把彩色图片转为灰度图片
-                region = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
-                # 4.获取图片信息熵
-                image_entropy += cal_image_entropy(region)
-                # 5.获取图片灰度值平均值
-                image_average += cal_average_gray(region)
-                # 6.获取图片的对比度
-                image_contrast += cal_contrast(region)
+                         node['inScreenBounds']['left']:node['inScreenBounds']['right']]
+                boolean_value, color_value = is_picture(region, node)
+                if boolean_value:
+                    #  (left, top),(right,bottom) 按照这个顺序，具体的数值通过在json文件里面进行获取
+                    draw_0 = cv2.rectangle(image, (node['inScreenBounds']['left'], node['inScreenBounds']['top']),
+                                           (node['inScreenBounds']['right'], node['inScreenBounds']['bottom']),
+                                           color_value, 3, 4, 0)
+                    h_s_v = cv2.cvtColor(region, cv2.COLOR_BGR2HSV)
+                    h, s, v = cv2.split(h_s_v)
+                    # 1.获取色调值
+                    h_value += round(np.sum(h), 2)
+                    # 2.获取饱和度值
+                    s_value += round(np.sum(s), 2)
+                    # 3.获取明度值
+                    v_value += round(np.sum(v), 2)
+                    # 把彩色图片转为灰度图片
+                    region = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+                    # 4.获取图片信息熵
+                    image_entropy += cal_image_entropy(region)
+                    # 5.获取图片灰度值平均值
+                    image_average += cal_average_gray(region)
+                    # 6.获取图片的对比度
+                    image_contrast += cal_contrast(region)
     print(round(h_value, 2), round(s_value, 2), round(v_value), image_entropy, image_average, image_contrast)
     # 显示画过矩形框的图片
     cv2.namedWindow('draw_0', 0)
@@ -119,6 +148,7 @@ def rectangle_and_get_information(json_file_, picture_file_):
     cv2.destroyWindow("draw_0")
 
 if __name__ == '__main__':
-    json_file = '20191202200057_20191202200055700_8.json'
-    picture_file = '20191202200057_20191202200055700.png'
+    str_ = '20191220161309499'
+    json_file = str_ + '.json'
+    picture_file = str_ + '.png'
     rectangle_and_get_information(json_file, picture_file)
