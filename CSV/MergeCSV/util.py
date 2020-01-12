0b2bbd572294817d352c4430731ab871e4b7a130
wangyan841331749 <841331749@qq.com>
2020-01-12 12:54:11 +0800

    添加了去除重复行的功能。

--- CSV/MergeCSV/util.py
+++ CSV/MergeCSV/util.py
@@ -9,8 +9,14 @@ import pandas as pd
 
 list_csv = ['data_1.csv', 'data_2.csv']
 tmp = pd.read_csv(list_csv[0])
-tmp.to_csv("all.csv", mode='a', index=False, header=True)
+tmp.to_csv("all.csv", mode='w', index=False, header=True)
 for i in list_csv[1:]:
     tmp = pd.read_csv(i)
+    # append的方式加入到csv文件中
     tmp.to_csv("all.csv", mode='a', index=False, header=False)
+tmp = pd.read_csv("all.csv")
+# 删除重复的行
+tmp = tmp.drop_duplicates()
+# index为False代表没有保存列索引，header为True代表保存列名称
+tmp.to_csv("all.csv", index=False, header=True)
 
